d7f2a4b2210db4682b652a28d298142a4e653b07
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2023-05-25 02:41:32 -0500

    changed files

--- app.py
+++ app.py
@@ -1,52 +1,163 @@
+import os
+import pandas as pd
+import numpy as np
+from tkinter import *
+from tkinter import filedialog
+import tkinter.font as font
+from functools import partial
+from pyresparser import ResumeParser
+from sklearn import datasets, linear_model 
 from flask import Flask, render_template
 from flask import url_for, request, session, redirect
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import bcrypt
-app = Flask(__name__)
+import numpy as np
+from functools import wraps
+import pickle
+import joblib
 
-app.config['MONGO_DBNAME'] = 'CareerPredictor'
+class train_model:
+    
+    def train(self):
+        data =pd.read_csv('sampledata/training_dataset.csv')
+        array = data.values
+
+        for i in range(len(array)):
+            if array[i][0]=="Male":
+                array[i][0]=1
+            else:
+                array[i][0]=0
+
+
+        df=pd.DataFrame(array)
+
+        maindf =df[[0,1,2,3,4,5,6]]
+        mainarray=maindf.values
+
+        temp=df[7]
+        train_y =temp.values
+        
+        self.mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg',max_iter =1000)
+        self.mul_lr.fit(mainarray, train_y)
+       
+    def test(self, test_data):
+        try:
+            test_predict=list()
+            for i in test_data:
+                test_predict.append(int(i))
+            y_pred = self.mul_lr.predict([test_predict])
+            return y_pred
+        except:
+            print("All Factors For Finding Personality Not Entered!")
+    def check_type(data):
+        if type(data)==str or type(data)==str:
+            return str(data).title()
+        if type(data)==list or type(data)==tuple:
+            str_list=""
+            for i,item in enumerate(data):
+                str_list+=item+", "
+            return str_list
+        else:   return str(data)
+
+   
+    def prediction_result(aplcnt_name, cv_path, personality_values):
+        applicant_data = {"Candidate Name":aplcnt_name.get(),  "CV Location":cv_path} 
+        age = personality_values[1]
+        print("\n############# Candidate Entered Data #############\n")
+        print(applicant_data, personality_values)
+        personality = model.test(personality_values)
+        print("\n############# Predicted Personality #############\n")
+        print(personality)
+        data = ResumeParser(cv_path).get_extracted_data()
+        try:
+            del data['name']
+            if len(data['mobile_number'])<10:
+                del data['mobile_number']
+        except:
+            pass
+        print("\n############# Resume Parsed Data #############\n")
+        for key in data.keys():
+            if data[key] is not None:
+                print('{} : {}'.format(key,data[key]))
+       
+    
+
+
+app = Flask(__name__)
+# pickleFile=open("model.pkl","rb")
+# regressor=pickle.load(pickleFile)
+# model = pickle.load(open(model1.pkl))
+# app.config['MONGO_DBNAME'] = 'carrerpredictorlogin'
 app.config['MONGO_URI'] = 'mongodb+srv://S555600:sobha1809@careerpredictor.ltr6ht6.mongodb.net/?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+# Database
+client = MongoClient('mongodb://localhost:27017/')
+db = client.careerpredictor
+db.careerpredictordata
+
+
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')  
+  return wrap
 
 @app.route('/')
 def home():
     return render_template('home.html')
     
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name' : request.form['username']})
+    # users = mongo.db.users
+    # login_user = users.find_one({'name' : request.form['username']})
 
-    if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
+    # if login_user:
+    #     if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+    #         session['username'] = request.form['username']
+    #         return redirect(url_for('dashboard'))
 
-    return 'Invalid username/password combination'
+    # return 'Invalid username/password combination'
+     return render_template('login.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        
-        return 'That username already exists!'
-
     return render_template('register.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/personalityprediction')
+@app.route('/personalityprediction', methods=['POST','GET'])
 def personalityprediction():
+    int_values = []
+    for x in request.form.values():
+        int_values.append(x)
+    aplcnt_name =  int_values[0]
+    # print(len(features))
+    cv_path =  int_values[3]
+    personality_values = [int_values[4],int_values[5],int_values[6],int_values[7],int_values[8]]
+    if __name__ == "__main__":
+        model = train_model()
+        model.train()
+        model.prediction_result(aplcnt_name, cv_path, personality_values)
     return render_template('personalityprediction.html')
+# if __name__ == '__main__':
+    
+#     app.run()
+@app.route('/result',methods = ['GET','POST'])
+def result():
+    if __name__ == "__main__":
+        model =  personalityprediction()       
+        model.prediction_result(aplcnt_name,cv_path, personality_values)
+    return render_template('result.html')
 
 @app.route('/jobprediction')
 def jobprediction():
@@ -55,6 +166,7 @@ def jobprediction():
 @app.route('/salaryprediction')
 def salaryprediction():
     return render_template('salaryprediction.html')
+
 @app.route('/loginRedirect')
 def loginRedirect():
     return render_template('loginRedirect.html')
