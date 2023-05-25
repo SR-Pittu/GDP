47ecb2829b5e0be06985d793dfcf57b0911481b9
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2023-05-24 20:29:55 -0500

    added files

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
 from flask import Flask, render_template
+from flask import url_for, request, session, redirect
+from flask_pymongo import PyMongo
+import bcrypt
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'CareerPredictor'
+app.config['MONGO_URI'] = 'mongodb+srv://S555600:sobha1809@careerpredictor.ltr6ht6.mongodb.net/?retryWrites=true&w=majority'
 
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/login')
+    
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('login.html')
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
 
-@app.route('/register')
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
     return render_template('register.html')
 
 @app.route('/dashboard')
