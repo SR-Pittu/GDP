6a753e7305360a22b9df9f86d24108cdbf007aec
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2023-10-11 16:15:22 -0500

    job prediction done

--- app.py
+++ app.py
@@ -16,10 +16,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+
 
 class train_model: 
     def __init__(self):
-        self.mul_lr = linear_model.LogisticRegression()
+        self.mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=1000)
     def train(self):
         data = pd.read_csv('sampledata/training_dataset.csv')
         array = data.values
@@ -33,14 +37,13 @@ class train_model:
         mainarray = maindf.values
         temp = df[7]
         trainy = temp
-        self.mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=1000)
         self.mul_lr.fit(mainarray, trainy)
 
     def test(self, test_data):
         nlp = spacy.load("en_core_web_sm")
         test_predict = list()
         for i in test_data:
-            test_predict.append(int(i))
+            test_predict.append(i)
         print(test_predict)
         y_pred = self.mul_lr.predict([test_predict])
         print(y_pred)
@@ -239,7 +242,74 @@ def result():
 @app.route('/jobprediction', methods=['POST','GET','PUT'])
 def jobprediction():
     if request.method =='POST':
-        return render_template("jobtitleresult.html",a = predict_job_title_from_cv())
+        list =[]
+        a1 = request.form.get("LogicalSkills")
+        list.append(int(a1)) # type: ignore
+        a2 = request.form.get("CodingSkills")
+        list.append(int(a2)) # type: ignore
+        a3 = request.form.get("Hackathons")
+        list.append(int(a3)) # type: ignore
+        a4 = request.form.get("PublicSpeaking")
+        list.append(int(a4)) # type: ignore
+        a5 = request.form.get('j')
+        list.append(checkValue(a5))
+        a6 = request.form.get('j1')
+        list.append(checkValue(a6))
+        a7 = request.form.get('j2')
+        list.append(checkValue(a7))
+        a8 = request.form.get('j3')
+        list.append(checkValue(a8))
+        a9 = request.form.get('j4')
+        list.append(checkValue(a9))
+        a10 = request.form.get('j5')
+        list.append(valuesCheck(a10))
+        a11 = request.form.get('j6')
+        list.append(valuesCheck(a11))
+        a12 = request.form.get('j7')
+        if(a12=="Hard"):
+            list.append(1)
+            list.append(0)
+        else:
+            list.append(0)
+            list.append(1)
+        a13 = request.form.get('j8')
+        if(a13=="Management"):
+            list.append(1)
+            list.append(0)
+        else:
+            list.append(0)
+            list.append(1)
+        a14 = request.form.get('j9')
+        list.append(a14)
+        a15 = request.form.get('j10')
+        list.append(a15)
+        a16 = request.form.get('j11') 
+        list.append(a16)
+        a17 = request.form.get('j12')
+        list.append(a17)
+        a18 = request.form.get('j13')
+        list.append(a18)
+        a19 = request.form.get('j14')
+        list.append(a19)
+        print(list)
+        string_to_code = {}
+        code_counter = 0
+        for item in list:
+            if isinstance(item, str):
+                if item not in string_to_code:
+                    string_to_code[item] = code_counter
+                    code_counter += 1
+
+        # Create a new list with the categorical codes
+        categorical_codes = [string_to_code.get(item, item) for item in list]
+
+        print(categorical_codes)
+        name = request.form.get("name")
+        print(name)
+        b = jobPredict()
+        b.train_decision_tree()
+        v = b.predict_decision_tree(categorical_codes)
+        return render_template("jobtitleresult.html",a=v, name= name)
     return render_template("jobprediction.html")
 
 @app.route('/jobtitleresult',methods = ['POST'])
@@ -252,10 +322,23 @@ def jobtitleresult():
 def salaryprediction():
     if request.method == 'POST':
         resume_path = "path_to_your_resume.pdf"
-        # job_title = extract_job_title(resume_path)
-        # print("Extracted Job Title:",job_title)
+        # job_title = 
+        print("Extracted Job Title:")
         return render_template('salaryprediction.html')
     return render_template('salaryprediction.html')
+
+def checkValue(c):
+    if(c=="Yes"):
+        return 1
+    else:
+        return 0
+def valuesCheck(b):
+    if(b=="Poor"):
+        return 0
+    elif(b=="Medium"):
+        return 1
+    else:
+        return 2
 
 
 def extract_skills_from_cv(cv_text):
@@ -268,74 +351,95 @@ def extract_skills_from_cv(cv_text):
             resume_tokens.append(token.text.lower())
 
     return resume_tokens
+class jobPredict:
+    def __init__(self):
+        self.dtree = DecisionTreeClassifier(random_state=1)
+        self.xgb = XGBClassifier(random_state=42, learning_rate=0.02, n_estimators=300)
+        self.df = pd.read_csv('sampledata/mldata.csv')
+        
+    def train_decision_tree(self):
+        df = pd.read_csv('sampledata/mldata.csv')
+        df.head()
+        cols = df[["self-learning capability?", "Extra-courses did","Taken inputs from seniors or elders", "worked in teams ever?", "Introvert"]]
+        for i in cols:
+            cleanup_nums = {i: {"yes": 1, "no": 0}}
+            df = df.replace(cleanup_nums) 
+        print("\n\nList of Categorical features: \n" , df.select_dtypes(include=['object']).columns.tolist())
+        mycol = df[["reading and writing skills", "memory capability score"]]
+        for i in mycol:
+            cleanup_nums = {i: {"poor": 0, "medium": 1, "excellent": 2}}
+            df = df.replace(cleanup_nums)
 
-def predict_job_title_from_cv():
-    df = pd.read_csv('sampledata/mldata.csv')
-    df.head()
-    cols = df[["self-learning capability?", "Extra-courses did","Taken inputs from seniors or elders", "worked in teams ever?", "Introvert"]]
-    for i in cols:
-        cleanup_nums = {i: {"yes": 1, "no": 0}}
-        df = df.replace(cleanup_nums)
+        category_cols = df[['certifications', 'workshops', 'Interested subjects', 'interested career area ', 'Type of company want to settle in?', 
+                            'Interested Type of Books']]
+        for i in category_cols:
+            df[i] = df[i].astype('category')
+            df[i + "_code"] = df[i].cat.codes # type: ignore
+
+        print("\n\nList of Categorical features: \n" , df.select_dtypes(include=['object']).columns.tolist())
+        df = pd.get_dummies(df, columns=["Management or Technical", "hard/smart worker"], prefix=["A", "B"])
+        df.head()
     
+        feed = df[['Logical quotient rating', 'coding skills rating', 'hackathons', 'public speaking points', 'self-learning capability?','Extra-courses did', 
+                'Taken inputs from seniors or elders', 'worked in teams ever?', 'Introvert', 'reading and writing skills', 'memory capability score',  
+                'B_hard worker', 'B_smart worker', 'A_Management', 'A_Technical', 'Interested subjects_code', 'Interested Type of Books_code', 'certifications_code', 
+                'workshops_code', 'Type of company want to settle in?_code',  'interested career area _code', 'Suggested Job Role']]
 
-    mycol = df[["reading and writing skills", "memory capability score"]]
-    for i in mycol:
-        cleanup_nums = {i: {"poor": 0, "medium": 1, "excellent": 2}}
-        df = df.replace(cleanup_nums)
+        # Taking all independent variable columns
+        df_train_x = feed.drop('Suggested Job Role',axis = 1)
 
-
-    # Label Encoding
-    category_cols = df[['certifications', 'workshops', 'Interested subjects', 'interested career area ', 'Type of company want to settle in?', 
-                        'Interested Type of Books']]
-    for i in category_cols:
-        df[i] = df[i].astype('category')
-        df[i + "_code"] = df[i].cat.codes # type: ignore
-    feed = df[['Logical quotient rating', 'coding skills rating', 'hackathons', 'public speaking points', 'self-learning capability?','Extra-courses did', 
-           'Taken inputs from seniors or elders', 'worked in teams ever?', 'Introvert', 'reading and writing skills', 'memory capability score',  
-            'Interested subjects_code', 'Interested Type of Books_code', 'certifications_code', 
-           'workshops_code', 'Type of company want to settle in?_code',  'interested career area _code',
-             'Suggested Job Role']]
+        # Target variable column
+        df_train_y = feed['Suggested Job Role']
     
-    # Taking all independent variable columns
-    df_train_x = feed.drop('Suggested Job Role',axis = 1)
+        x_train, x_test, y_train, y_test = train_test_split(df_train_x, df_train_y, test_size=0.20, random_state=42)
 
-    # Target variable column
-    df_train_y = feed['Suggested Job Role']
+        # dtree = DecisionTreeClassifier(random_state=1)
+        dtree = self.dtree.fit(x_train, y_train)
 
-    # Train-Test Splitting
-    x_train, x_test, y_train, y_test = train_test_split(df_train_x, df_train_y, test_size=0.20, random_state=42)
+        y_pred = dtree.predict(x_test)
+        cm = confusion_matrix(y_test,y_pred)
+        accuracy = accuracy_score(y_test,y_pred)
+        print("confusion matrics=",cm)
+        print("accuracy=",accuracy*10)
+        userdata = [['7','6','6','8','3','5','4', '4', '7', '3', '3', '6','8','7','5','7','4','5','6','8','8']]
+        ynewclass = dtree.predict(userdata)
+        ynew = dtree.predict_proba(userdata)
+        print(ynewclass)
+        print("Probabilities of all classes: ", ynew)
+        print("Probability of Predicted class : ", np.max(ynew))
 
-    #Random Forest Classifier
-    clf3 = RandomForestClassifier(n_estimators=100) 
-    clf3 = clf3.fit(x_train, y_train)
-   
-    categorical_col = df[['self-learning capability?', 'Extra-courses did','reading and writing skills', 'memory capability score', 
-                      'Taken inputs from seniors or elders', 'Management or Technical', 'hard/smart worker', 'worked in teams ever?', 
-                      'Introvert', 'interested career area ']]
-    for i in categorical_col:
-        print(df[i].value_counts(), end="\n\n")
+    def train_xgboost(self):
+        feed = self.df[['Logical quotient rating', 'coding skills rating', 'hackathons', 'public speaking points', 'self-learning capability?','Extra-courses did', 
+                'Taken inputs from seniors or elders', 'worked in teams ever?', 'Introvert', 'reading and writing skills', 'memory capability score',  
+                'B_hard worker', 'B_smart worker', 'A_Management', 'A_Technical', 'Interested subjects_code', 'Interested Type of Books_code', 'certifications_code', 
+                'workshops_code', 'Type of company want to settle in?_code',  'interested career area _code', 'Suggested Job Role']]
 
-# Taking all independent variable columns
-    df_train_x = feed.drop('Suggested Job Role',axis = 1)
+        df_train_x = feed.drop('Suggested Job Role',axis = 1)
 
-    # Target variable column
-    df_train_y = feed['Suggested Job Role']
+        # Target variable column
+        df_train_y = feed['Suggested Job Role']
+    # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(df_train_x, df_train_y, test_size=0.2, random_state=42)
 
-    x_train, x_test, y_train, y_test = train_test_split(df_train_x, df_train_y, test_size=0.20, random_state=42)
+        # Encode the string labels to numeric labels
+        label_encoder = LabelEncoder()
+        y_train_encoded = label_encoder.fit_transform(y_train)
+        y_test_encoded = label_encoder.transform(y_test)
 
-    dtree = DecisionTreeClassifier(random_state=1)
-    dtree = dtree.fit(x_train, y_train)
-
-    y_pred = dtree.predict(x_test)
-    cm = confusion_matrix(y_test,y_pred)
-    accuracy = accuracy_score(y_test,y_pred)
-    print("confusion matrics=",cm)
-    print("  ")
-    print("accuracy=",accuracy*10)
-    userdata = [['7','6','6','8','3','5','4', '4', '7', '3', '3', '6','8', 
-                    ,'7','4','5','6','8','8']]
-    ynewclass = dtree.predict(userdata)
-    ynew = dtree.predict_proba(userdata)
-    print(ynewclass)
-    return ynewclass
-    
+        df_train_x = feed.drop('Suggested Job Role',axis = 1)
+        df_train_y = feed['Suggested Job Role']
+        xgb = XGBClassifier(random_state = 42, learning_rate=0.02, n_estimators=300)
+        xgb.fit(X_train.values, y_train_encoded)
+        xgb_y_pred = xgb.predict(X_test)
+        xgb_cm = confusion_matrix(y_test_encoded, xgb_y_pred)
+        xgb_accuracy = accuracy_score(y_test_encoded, xgb_y_pred)
+        print("confusion matrics=",xgb_cm)
+        print(xgb_y_pred)
+        print("accuracy=",xgb_accuracy*10)
+    def predict_decision_tree(self, data):
+        # Use the self.dtree model to predict job titles for input data
+        for i in data:
+            i = str(i)
+        print(data)
+        value = self.dtree.predict([data])        
+        return value
